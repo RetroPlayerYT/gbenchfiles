a4dd57b04491424a1ae4b88a7259083f7333b13e
Retro <59291834+RetroPlayerYT@users.noreply.github.com>
2022-04-15 22:10:30 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@ echo Greenbench
 echo
 echo Type "bench list" to see the list of the mods and ressource packs.                                                                         
 echo Type "bench help" to see the list of all the commands.
-SET /P M=Type in the number beside the Mincraft Launcher you want and then press ENTER:  
+SET /P M=Please select a mod.
 IF %M%==bench list start (name of file)
 IF %M%==bench help start (name of file)
 IF %M%==gbench://retroakame/aetherretexture start https://gl.githack.com/rodrigosanchopires/gbenchmods/-/raw/master/Aether_Texture_Pack.zip
